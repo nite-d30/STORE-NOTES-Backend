16fb6a99b191a42891e64f0d7a4ea42273acf0d2
niks896 <desai96nikhil@gmail.com>
2020-06-27 16:45:05 +0530

    backend

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authverify=require('./authverify');
-const {getrooms,postrooms,updaterooms}=require('../controllers/rooms');
+const {getrooms,postrooms,updaterooms,roomavailability}=require('../controllers/rooms');
 
 
 router.get('/getrooms',authverify,getrooms);
 router.post('/storerooms',authverify,postrooms);
 router.put('/updaterooms',authverify,updaterooms);
+router.post('/roomavailability',authverify,roomavailability);
+
 
 module.exports = router;
